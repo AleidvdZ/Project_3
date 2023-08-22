c55f4eaab5d233ac77c42a2822b07257a27bd84c
AleidvdZ <131220504+AleidvdZ@users.noreply.github.com>
2023-08-22 17:31:42 -0500

    0822 Flask file updated, jsonify not working

--- starter_files/Flask/app_solution_example.py
+++ starter_files/Flask/app_solution_example.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 # Database Setup
-engine = create_engine("sqlite:///bank_data.sqlite")
+engine = create_engine("sqlite:///bankdata.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -18,49 +18,44 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # # Save reference to the table
-# Banks = Base.classes.banks
+Bankdata = Base.classes.bankdata
 
 # Flask Setup
 app = Flask(__name__)
 
 # Flask Routes
 
+
 @app.route('/')
-def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+# def home():
+#     return render_template('index.html')
 
 
-# still a little confused on how the data ('/bank_data') and ('/banklist') is callin in for js through d3.json
-# structure of the folders is important for the references (like "template")
+# if __name__ == '__main__':
+#     app.run()
 
-# This still needs editing and moving around - WILL NOT WORK THIS way AvdZ 8/21
+# @app.route("/api/v1.0/banks")
+def banks():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
+    """Return a list of bank names"""
+    # Query all bankds
+    results = session.query(Bankdata.name).all()
 
+    session.close()
 
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_banks = []
+    for name in results:
+        banks_dict = {}
+        banks_dict["name"] = name
+        all_banks.append(banks_dict)
+   
+        print(all_banks)
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-#     session.close()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
+   #  return jsonify(all_banks)
+   
 
 
 if __name__ == '__main__':
