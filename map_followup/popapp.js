fb6285029463cb605ff4de8e620dfe88f449bf77
AleidvdZ <131220504+AleidvdZ@users.noreply.github.com>
2023-08-27 21:03:19 -0500

    0827 pop graph add

---
+++ map_followup/popapp.js
@@ -0,0 +1,350 @@
+//Data for the states
+let states = 
+[
+  {
+    state: "Alaska",
+    location: [61.3850,-152.2683],
+    banks_closed: 0,
+    population: 733583
+  },
+  {
+    state: "Alabama",
+    location: [32.7990,-86.8073],
+    banks_closed: 7,
+    population: 5074296
+  },
+  {
+    state: "Arkansas",
+    location: [34.9513,-92.3809],
+    banks_closed: 4,
+    population: 3045637
+  },
+  {
+    state: "Arizona",
+    location: [33.7712,-111.3877],
+    banks_closed: 16,
+    population: 7359197
+  },
+  {
+    state:"California",
+    location: [36.1700,-119.7462],
+    banks_closed: 43,
+    population: 39029342
+  },
+  {
+    state:"Colorado",
+    location: [39.0646,-105.3272],
+    banks_closed: 10,
+    population: 5839926
+  },
+  {
+    state:"Connecticut",
+    location: [341.5834,-72.7622],
+    banks_closed: 2,
+    population: 3626205
+  },
+  {
+    state:"Delaware",
+    location: [39.3498,-75.5148],
+    banks_closed: 0,
+    population: 1018396
+  },
+  {
+    state:"Florida",
+    location: [27.8333,-81.7170],
+    banks_closed: 76,
+    population: 22244823
+  },
+  {
+    state:"Georgia",
+    location: [32.9866,-83.6487],
+    banks_closed: 93,
+    population: 10912876
+  },
+  {
+    state:"Hawaii",
+    location: [21.1098,-157.5311],
+    banks_closed: 1,
+    population: 1440196
+  },
+  {
+    state:"Iowa",
+    location: [42.0046,-93.2140],
+    banks_closed: 2,
+    population: 3200517
+  },
+  {
+    state:"Idaho",
+    location: [44.2394,-114.5103],
+    banks_closed: 2,
+    population: 1939033
+  },
+  {
+    state:"Illinois",
+    location: [40.3363,-89.0022],
+    banks_closed: 69,
+    population: 12582032
+  },
+  {
+    state:"Indiana",
+    location: [39.8647,-86.2604],
+    banks_closed: 3,
+    population: 6833037
+  },
+  {
+    state:"Kansas",
+    location: [38.5111,-96.8005],
+    banks_closed: 12,
+    population: 2937150
+  },
+  {
+    state:"Kentucky",
+    location: [37.6690,-84.6514],
+    banks_closed: 3,
+    population: 4512310
+  },
+  {
+    state:"Louisiana",
+    location: [31.1801,-91.8749],
+    banks_closed: 4,
+    population: 4590241
+  },
+  {
+    state:"Massachusetts",
+    location: [42.2373,-71.5314],
+    banks_closed: 1,
+    population: 6981974
+  },
+  {
+    state:"Maryland",
+    location: [39.0724,-76.7902],
+    banks_closed: 10,
+    population: 6164660
+  },
+  {
+    state:"Maine",
+    location: [44.6074,-69.3977],
+    banks_closed: 0,
+    population: 1385340
+  },
+  {
+    state:"Michigan",
+    location: [43.3504,-84.5603],
+    banks_closed: 14,
+    population: 10034113
+  },
+  {
+    state:"Minnesota",
+    location: [45.7326,-93.9196],
+    banks_closed: 23,
+    population: 5717184
+  },
+  {
+    state:"Missouri",
+    location: [38.4623,-92.3020],
+    banks_closed: 16,
+    population: 6177957
+  },
+  {
+    state:"Mississippi",
+    location: [32.7673,-89.6812],
+    banks_closed: 2,
+    population: 2940057
+  },
+  {
+    state:"Montana",
+    location: [46.9048,-110.3261],
+    banks_closed: 0,
+    population: 1122867
+  },
+  {
+    state:"North Carolina",
+    location: [35.6411,-79.8431],
+    banks_closed: 7,
+    population: 10698973
+  },
+  {
+    state:"North Dakota",
+    location: [47.5362,-99.7930],
+    banks_closed: 0,
+    population: 779261
+  },
+  {
+    state:"Nebraska",
+    location: [41.1289,-98.2883],
+    banks_closed: 4,
+    population: 1967923
+  },
+  {
+    state:"New Hampshire",
+    location: [43.4108,-71.5653],
+    banks_closed: 1,
+    population: 1395231
+  },
+  {
+    state:"New Jersey",
+    location: [40.3140,-74.5089],
+    banks_closed: 8,
+    population: 9261699
+  },
+  {
+    state:"New Mexico",
+    location: [34.8375,-106.2371],
+    banks_closed: 3,
+    population: 2113344
+  },
+  {
+    state:"Nevada",
+    location: [38.4199,-117.1219],
+    banks_closed: 12,
+    population: 3177772
+  },
+  {
+    state:"New York",
+    location: [42.1497,-74.9384],
+    banks_closed: 6,
+    population: 19677151
+  },
+  {
+    state:"Ohio",
+    location: [40.3736,-82.7755],
+    banks_closed: 9,
+    population: 11756058
+  },
+  {
+    state:"Oklahoma",
+    location: [35.5376,-96.9247],
+    banks_closed: 7,
+    population: 4019800
+  },
+  {
+    state:"Oregon",
+    location: [44.5672,-122.1269],
+    banks_closed: 6,
+    population: 4240137
+  },
+  {
+    state:"Pennsylvania",
+    location: [40.5773,-77.2640],
+    banks_closed: 10,
+    population: 12972008
+  },
+  {
+    state:"Rhode Island",
+    location: [41.6772,-71.5101],
+    banks_closed: 0,
+    population: 1093734
+  },
+  {
+    state:"South Carolina",
+    location: [33.8191,-80.9066],
+    banks_closed: 10,
+    population: 5282634
+  },
+  {
+    state:"South Dakota",
+    location: [44.2853,-99.4632],
+    banks_closed: 1,
+    population: 909824
+  },
+  {
+    state:"Tennessee",
+    location: [35.7449,-86.7489],
+    banks_closed: 7,
+    population: 7051339
+  },
+  {
+    state:"Texas",
+    location: [31.1060,-97.6475],
+    banks_closed: 13,
+    population: 30029572
+  },
+  {
+    state:"Utah",
+    location: [40.1135,-111.8535],
+    banks_closed: 8,
+    population: 3380800
+  },
+  {
+    state:"Virginia",
+    location: [37.7680,-78.2057],
+    banks_closed: 5,
+    population: 8683619
+  },
+  {
+    state:"Vermont",
+    location: [44.0407,-72.7093],
+    banks_closed: 0,
+    population: 647064
+  },
+  {
+    state:"Washington",
+    location: [47.3917,-121.5708],
+    banks_closed: 19,
+    population: 7785786
+  },
+  {
+    state:"Wisconsin",
+    location: [44.2563,-89.6385],
+    banks_closed: 11,
+    population: 5892539
+  },
+  {
+    state:"West Virginia",
+    location: [38.4680,-80.9696],
+    banks_closed: 2,
+    population: 1775156
+  },
+  {
+    state:"Wyoming",
+    location: [42.7475,-107.2085],
+    banks_closed: 1,
+    population: 581381
+  }
+]
+
+let stateList = []
+let populationList = []
+let banksClosed = []
+let populationDiv = []
+
+// For loop to populate arrays
+for (let i = 0; i < states.length; i++) {
+  let row = states[i];
+  stateList.push(row.state);
+  populationList.push(row.population);
+  banksClosed.push(row.banks_closed);
+}
+
+// For loop to populate arrays
+for (let i = 0; i < populationList.length; i++) {
+  let div = populationList[i]/1000000;
+  populationDiv.push(div);
+}
+
+// Trace1 for the Greek Data
+let trace1 = {
+  x: stateList,
+  y: banksClosed,
+  name: "Banks Closed",
+  type: "bar"
+};
+
+// Trace 2 for the Roman Data
+let trace2 = {
+  x: stateList,
+  y: populationDiv,
+  name: "State Population (x 1 million)",
+  type: "scatter"
+};
+
+// Create data array
+let data = [trace1, trace2];
+
+// Apply a title to the layout
+let layout = {
+  title: "Number of Banks Closed vs. State Population Size",
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("popPlot", data, layout);
